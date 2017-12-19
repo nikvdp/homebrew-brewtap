9f46aaf3efebecca104d9f406d169ddb4cab9e75
Nik V <nik@nikvdp.com>
2017-12-18 16:52:46 -0800

    Add bottle to lemonade formula

--- Formula/lemonade.rb
+++ Formula/lemonade.rb
@@ -2,11 +2,16 @@ require "language/go"
 
 class Lemonade < Formula
   desc "Lemonade remote utility tool"
-  # homepage "https://github.com/pocke/lemonade"
+  homepage "https://github.com/pocke/lemonade"
   url "https://github.com/pocke/lemonade/archive/v1.1.1.zip"
   version '1.1.1'
   sha256 "d51a78f51bf0504e8c84c91f962859b25ba14e469f2e3dffcf8d7da495667cbb"
-  # revision 1
+
+  bottle do
+    root_url "http://nikvdp.com/homebrew-bottles"
+    cellar :any_skip_relocation
+    sha256 "7a3eeb19e2afce398ccfaefbd71b61d7fc99644e8228916e2dde12ebfa2cb703" => :el_capitan
+  end
 
   head "https://github.com/pocke/lemonade.git"
 
